d55aca4d9e8820ef8c31c9acf48ff5d5a78871ba
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2024-06-26 11:18:21 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environmentProd = {
+export const environment = {
     production: true,
     urlApi: 'http://api.floreriaenvios.com/api',
     mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
