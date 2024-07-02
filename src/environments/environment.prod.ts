c4472146c979945e1733f176bd122935282a1008
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2024-07-02 12:11:04 -0500

    Configuración para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    urlApi: 'https://api.floreriaenvios.com/api',
+    urlApi: 'https://api.floreriaenvios.com:8001/api',
     mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
     paypalClientId: 'AWURsRyryiY0BgnQyF1jG0TMWbS1JnGp7q82POpJ7GvrwJT19v9JdYgU7KZxhG4EnbKZ_BZ-9SceicBY'
 };
